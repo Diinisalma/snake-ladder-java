15ccbab001516f5fec20491e549c928127320278
Diini Salma <op44@fifgroup.co.id>
2025-02-18 14:50:29 +0700

    chore(snake): Adjustment unit testing in snake class

--- src/test/java/io/github/diinisalma/snake/SnakeTest.java
+++ src/test/java/io/github/diinisalma/snake/SnakeTest.java
@@ -1,25 +1,39 @@
 package io.github.diinisalma.snake;
 
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-@RunWith(JUnitPlatform.class)
-@SelectPackages("io.github.diinisalma.snake")
 public class SnakeTest {
 
-    @Test
-    public void given_head_value_100_or_more_then_throw_error() {
-        RuntimeException exception = Assertions.assertThrows(RuntimeException.class, () -> new Snake(101, 1));
-        Assertions.assertEquals("Invalid head position", exception.getMessage());
+    @ParameterizedTest
+    @ValueSource(ints = {100, 101, 1})
+    public void setTail_given_tail_invalid_value_then_throw_error(int tail) {
+        Snake snake = new Snake(1, 10);
+        RuntimeException exception = Assertions.assertThrows(RuntimeException.class, () -> snake.setTail(tail));
+        Assertions.assertEquals("Invalid tail position", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {100, 101, 1})
+    public void given_tail_invalid_value_then_throw_error(int tail) {
+        RuntimeException exception = Assertions.assertThrows(RuntimeException.class, () -> new Snake(1, tail));
+        Assertions.assertEquals("Invalid tail position", exception.getMessage());
     }
 
     @Test
-    public void setHead_given_head_value_100_or_more_then_throw_error() {
-        Snake snake = new Snake(10, 1);
-        RuntimeException exception = Assertions.assertThrows(RuntimeException.class, () -> snake.setHead(101));
-        Assertions.assertEquals("Invalid head position", exception.getMessage());
+    public void given_tail_valid_value_then_set_tail() {
+        Snake snake = new Snake(1, 2);
+        Assertions.assertEquals(2, snake.getTail());
     }
+
+    @Test
+    public void setTail_given_tail_valid_value_then_set_tail() {
+        Snake snake = new Snake(1, 2);
+        snake.setTail(3);
+        Assertions.assertEquals(3, snake.getTail());
+    }
+    
+    
 }
